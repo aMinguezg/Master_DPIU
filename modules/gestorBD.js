9c14320aeafad9cf10b88f5d196766df71374307
aMinguezg <44700540+aMinguezg@users.noreply.github.com>
2019-01-10 11:01:04 +0100

    gestorBD

---
+++ modules/gestorBD.js
@@ -0,0 +1,178 @@
+module.exports = {
+    mongo : null,
+    app : null,
+    init : function(app, mongo) {
+        this.mongo = mongo;
+        this.app = app;
+    },
+    obtenerArticulosPg : function(criterio,pg,funcionCallback){
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                
+                var collection = db.collection('articulos');
+                collection.count(function(err, count){
+    
+                    collection.find(criterio).skip( (pg-1)*4 ).limit( 4 )
+                        .toArray(function(err, articulos) {
+                            
+                        if (err) {
+                            funcionCallback(null);
+                        } else {
+                            funcionCallback(articulos, count);
+                        }
+                        db.close();
+                    });
+                    
+                });
+            }
+        });
+    },
+    
+    obtenerCompras : function(criterio,funcionCallback){
+		this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+			if (err) {
+				funcionCallback(null);
+			} else {
+				
+				var collection = db.collection('compras');
+				collection.find(criterio).toArray(function(err, usuarios) {
+					if (err) {
+						funcionCallback(null);
+					} else {
+						funcionCallback(usuarios);
+					}
+					db.close();
+				});
+			}
+		});
+	},
+
+    insertarCompra: function(compra, funcionCallback) {
+		this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+			if (err) {
+				funcionCallback(null);
+			} else {
+				var collection = db.collection('compras');
+				collection.insert(compra, function(err, result) {
+					if (err) {
+						funcionCallback(null);
+					} else {
+						funcionCallback(result.ops[0]._id);
+					}
+					db.close();
+				});
+			}
+		});
+	},
+
+    eliminarArticulo : function(criterio, funcionCallback) {
+		this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+			if (err) {
+				funcionCallback(null);
+			} else {
+				var collection = db.collection('articulos');
+				collection.remove(criterio, function(err, result) {
+					if (err) {
+						funcionCallback(null);
+					} else {
+						funcionCallback(result);
+					}
+					db.close();
+				});
+			}
+		});
+	},
+
+    insertarUsuario : function(usuario, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                var collection = db.collection('usuarios');
+                collection.insert(usuario, function(err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+
+    obtenerUsuarios : function(criterio,funcionCallback){
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+
+                var collection = db.collection('usuarios');
+                collection.find(criterio).toArray(function(err, usuarios) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(usuarios);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+
+    obtenerArticulos : function(criterio, funcionCallback){
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                var collection = db.collection('articulos');
+                collection.find(criterio).toArray(function(err, articulos) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(articulos);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+
+    insertarArticulo : function(cancion, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                var collection = db.collection('articulos');
+                collection.insert(cancion, function(err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+
+    modificarArticulo : function(criterio, articulo, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                var collection = db.collection('articulos');
+                collection.update(criterio, {$set: articulo}, function(err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+};
